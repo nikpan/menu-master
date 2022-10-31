189b793e8db96cd1fac545872f96c97a5e94df74
nikpan <nikhil.pangarkar@gmail.com>
2022-10-30 22:34:10 -0700

    Updates

---
+++ precache-manifest.13d2471f996345e71d8db2375432329c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d0617e7dfa3e49bc7be",
+    "url": "/menu-master/static/css/main.5a586e28.chunk.css"
+  },
+  {
+    "revision": "2d0617e7dfa3e49bc7be",
+    "url": "/menu-master/static/js/main.afaec51e.chunk.js"
+  },
+  {
+    "revision": "3910523e0cb8dcf1b30f",
+    "url": "/menu-master/static/js/runtime~main.97451312.js"
+  },
+  {
+    "revision": "c7a5ded80c5856937289",
+    "url": "/menu-master/static/js/2.66ece21c.chunk.js"
+  },
+  {
+    "revision": "b80c4abeebcf3582831c872aee70e625",
+    "url": "/menu-master/index.html"
+  }
+];
